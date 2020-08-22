e7f3a02c769b8fb75a56d9ec6011e579cef0dc0d
Paul Muhl <pmuhl@sbunified.org>
2020-08-22 16:04:34 -0700

    forgot SocialMedia.swift

---
+++ OliveAndGold/SocialMedia.swift
@@ -0,0 +1,18 @@
+//
+//  socialMedia.swift
+//  OliveAndGoldFP3
+//
+//  Created by Mobile on 1/13/20.
+//  Copyright © 2020 Mobile. All rights reserved.
+//
+
+import Foundation
+
+
+struct SocialMedia{
+    var facebook = "https://www.facebook.com"
+    var instagram = "https://www.instagram.com/?hl=en"
+    var twitter = "https://twitter.com"
+    var SBHSWebsite = "https://sbhs.sbunified.org"
+    
+}
